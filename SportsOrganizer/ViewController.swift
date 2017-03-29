be14fae216bb984b6b13fe56a67cb57286139794
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-03-29 22:37:54 +0200

    Initial Commit

---
+++ SportsOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportsOrganizer
+//
+//  Created by Bratislav Ljubisic on 3/29/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
