99ef5728128287a2b07913c58691523ad380ccd1
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-04-08 20:27:46 +0200

    Model implementation

--- SportsOrganizer/Model/SportsOrganizerModel.swift
+++ SportsOrganizer/Model/SportsOrganizerModel.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import Starscream
+import RxSwift
+
+public class SportsOrganizerModel: SOModelProtocol {
+    
+    var socket: WebSocket
+    var textSubject: Variable<String>
+    
+    init() {
+        socket = WebSocket(url: URL(string: "ws://echo.websocket.org")!)
+        textSubject = Variable("Start")
+    }
+    
+    func send(message: Message) {
+        socket.write(string: message.message)
+    }
+    
+    func create(message: Message) -> Bool {
+        return true
+    }
+    
+}
+
+extension SportsOrganizerModel: WebSocketDelegate {
+    
+    public func websocketDidConnect(socket: Starscream.WebSocket) {
+        
+    }
+    
+    public func websocketDidDisconnect(socket: Starscream.WebSocket, error: NSError?) {
+        
+    }
+    
+    public func websocketDidReceiveMessage(socket: Starscream.WebSocket, text: String) {
+        textSubject.value = text
+    }
+    
+    public func websocketDidReceiveData(socket: Starscream.WebSocket, data: Data) {
+        
+    }
+}
