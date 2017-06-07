bfd288402788cb8a06125eca635f83e4dcf11246
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-06-07 16:11:40 +0200

    This is CommunicationProtocol implementation for Web Socket

--- SportsOrganizer/Model/WebSocketCommunication.swift
+++ SportsOrganizer/Model/WebSocketCommunication.swift
@@ -10,16 +10,49 @@ import Foundation
 import Starscream
 import RxSwift
 
-public class WebSocketCommunication: CommunicationProtocol {
+public class WebSocketCommunication: CommunicationProtocol  {
     
     var socket: WebSocket
     
     init(withURL url: URL) {
-        socket =  WebSocket(url)
+        socket =  WebSocket(url: url)
     }
     
+    func connect() {
+        self.socket.delegate = self
+        self.socket.disableSSLCertValidation = true
+        self.socket.connect()
+    }
     
+    func send(Message message: Message) -> Bool {
+        socket.write(string: message.message)
+        return true
+    }
     
+    func status() -> CommunicationStatus {
+        return .connected
+    }
+}
+
+extension WebSocketCommunication: WebSocketDelegate {
+    
+    public func websocketDidConnect(socket: WebSocket) {
+        print("Connected")
+    }
+    
+    public func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+        print("Disconnected: error: \(error.debugDescription)")
+    }
+    
+    public func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+        print("Received text: \(text)")
+        //textSubject.value = text
+    }
+    
+    public func websocketDidReceiveData(socket: WebSocket, data: Data) {
+        print("Received data: \(data)")
+        
+    }
     
 }
 
