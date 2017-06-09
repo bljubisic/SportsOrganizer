e7a392987158e02200631649481faf73d83d75a7
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-06-09 23:49:44 +0200

    State is now tracked within model, and textSubject is connected to messagesData

--- SportsOrganizer/Model/SportsOrganizerModel.swift
+++ SportsOrganizer/Model/SportsOrganizerModel.swift
@@ -14,12 +14,22 @@ public class SportsOrganizerModel: SOModelProtocol {
     
     var communicationPortal: CommunicationProtocol
     var textSubject: Variable<CommMessage>
+    var state: State
     
     init() {
         
         communicationPortal = WebSocketCommunication(withURL: URL(string: "wss://localhost:8444/ws/app")!)
+        self.communicationPortal.shouldReconnect(flag: true)
         textSubject = Variable(CommMessage(message: Data(), state: .idle))
         self.communicationPortal.connect()
+        self.state = .idle
+        
+        
+        _ = self.communicationPortal.messagesData.asObservable().map{ (data) -> CommMessage in
+            return CommMessage(message: data, state: .initialState)
+            }.subscribe(onNext: { (message) in
+                self.textSubject.value = message
+            })
     }
     
     func send(message: Message) {
