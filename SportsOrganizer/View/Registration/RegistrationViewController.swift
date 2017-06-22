984b442920c5010d0439f9f34a2eb8be05664add
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-06-22 16:01:34 +0200

    Added RegistrationViewController

---
+++ SportsOrganizer/View/Registration/RegistrationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegistrationViewController.swift
+//  SportsOrganizer
+//
+//  Created by Bratislav Ljubisic on 6/21/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    
+    var tableView: UITableView!
+    var viewModel: RegViewModelProtocol!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
