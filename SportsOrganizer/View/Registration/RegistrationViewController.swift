749b1cab89782f299d35d99debb4ce5088f700bc
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-06-27 12:32:18 +0200

    Added simple UI for registration

--- SportsOrganizer/View/Registration/RegistrationViewController.swift
+++ SportsOrganizer/View/Registration/RegistrationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class RegistrationViewController: UIViewController {
     
@@ -15,8 +16,32 @@ class RegistrationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.tableView = UITableView()
+        self.view.addSubview(self.tableView)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
+}
+
+extension RegistrationViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100.0
+    }
+}
+
+extension RegistrationViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+    }
 }
