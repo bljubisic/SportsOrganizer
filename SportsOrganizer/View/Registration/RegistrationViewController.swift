f6258ac3ff29b099e3c46b8c23787699ff016181
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-07-25 16:15:41 +0200

    Added FirstName table view cell with RxSwift subscriptions

--- SportsOrganizer/View/Registration/RegistrationViewController.swift
+++ SportsOrganizer/View/Registration/RegistrationViewController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class RegistrationViewController: UIViewController {
     
     var tableView: UITableView!
     var viewModel: RegViewModelProtocol!
+    var cells: [UITableViewCell]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView = UITableView()
+    
         self.view.addSubview(self.tableView)
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        self.tableView.register(FirstNameTableViewCell.self, forCellReuseIdentifier: "firstname")
+        
+        self.cells = [self.tableView.dequeueReusableCell(withIdentifier: "firstname")!]
         // Do any additional setup after loading the view.
     }
 
@@ -34,14 +41,28 @@ extension RegistrationViewController: UITableViewDelegate {
 
 extension RegistrationViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 6
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let cell = self.cells[indexPath.row]
+        let disposeBag: DisposeBag = DisposeBag()
+        if(indexPath.row == 0) {
+            let observable: Observable<String?> = (cell as! FirstNameTableViewCell).firstNameTextField.rx.text.asObservable()
+            observable.subscribe(onNext: {(string: String?) in
+                print(string!)
+            },
+                                 onCompleted: {
+                                    
+                                    
+            }).addDisposableTo(disposeBag)
+        }
+        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // Each cell will have same height except the ones with date pickers
         
+        return 100.0
     }
 }
