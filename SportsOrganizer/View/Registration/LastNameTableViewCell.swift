35ca734d8f1dd18344b58e4fafcdd52d606bf615
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-08-14 15:06:53 +0200

    new table view cell for last name

---
+++ SportsOrganizer/View/Registration/LastNameTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  LastNameTableViewCell.swift
+//  SportsOrganizer
+//
+//  Created by Bratislav Ljubisic on 7/27/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class LastNameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    var lastNameTextField: UITextField!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        lastNameTextField = UITextField()
+        contentView.addSubview(lastNameTextField)
+        lastNameTextField.snp.makeConstraints { (make) in
+            make.top.equalTo(self.contentView.snp.top).inset(-5.0)
+            make.bottom.equalTo(self.contentView.snp.bottom).inset(-5.5)
+            make.leading.equalTo(self.contentView.snp.leading).inset(-8.0)
+            make.trailing.equalTo(self.contentView.snp.trailing).inset(-8.0)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
