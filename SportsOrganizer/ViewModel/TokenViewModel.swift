61625f0b7fdaaf46693c8f1ad3293ccdb7da418f
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2017-09-25 09:48:13 +0200

    TokenViewModel implementation

---
+++ SportsOrganizer/ViewModel/TokenViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  TokenViewModel.swift
+//  SportsOrganizer
+//
+//  Created by Bratislav Ljubisic on 9/23/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+final class TokenViewModel: TokenViewModelInputs, TokenViewModelOutputs, TokenViewModelProtocol {
+    var phoneNumber: String
+    
+    func createTokenMessage() -> TokenMessage {
+        return TokenMessage(phone: phoneNumber, token: token)
+    }
+    
+    func sendToken(Message message: TokenMessage) -> Bool {
+        model.sendToken(message: message)
+        return true
+    }
+    
+    var messageObservable: Observable<CommMessage>
+    
+    var token: String = ""
+    var model: SOModelProtocol
+    
+    var outputs: TokenViewModelOutputs {
+        return self
+    }
+    
+    var inputs: TokenViewModelInputs {
+        return self
+    }
+    
+    init(withModel model: SOModelProtocol, andPhone phone: String) {
+        self.model = model
+        self.phoneNumber = phone
+        self.messageObservable = model.textSubject
+    }
+    
+}
