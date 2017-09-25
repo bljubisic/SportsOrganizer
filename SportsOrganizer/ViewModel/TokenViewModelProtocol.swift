1a90bd7510a2b20894ae375c15bc83966278d540
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2017-09-25 09:46:44 +0200

    Added TokenViewModel protocol

---
+++ SportsOrganizer/ViewModel/TokenViewModelProtocol.swift
@@ -0,0 +1,27 @@
+//
+//  TokenViewModelProtocol.swift
+//  SportsOrganizer
+//
+//  Created by Bratislav Ljubisic on 9/23/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol TokenViewModelOutputs {
+    var messageObservable: Observable<CommMessage> { get }
+    var phoneNumber: String { get }
+}
+
+protocol TokenViewModelInputs {
+    func createTokenMessage() -> TokenMessage
+    func sendToken(Message message: TokenMessage) -> Bool
+}
+
+protocol TokenViewModelProtocol {
+    var token: String { set get }
+    var model: SOModelProtocol { get }
+    var outputs: TokenViewModelOutputs { get }
+    var inputs: TokenViewModelInputs { get }
+}
